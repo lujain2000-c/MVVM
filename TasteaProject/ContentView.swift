53c329b60194e062d230ea0a4c26141899e95112
LUJAIN ALKANHAL <lujain.i.k20@gmail.com>
2023-05-23 10:40:46 +0300

    Initial Commit

---
+++ TasteaProject/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TasteaProject
+//
+//  Created by لجين إبراهيم الكنهل on 03/11/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TasteaProject/TasteaProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasteaProjectApp.swift
+//  TasteaProject
+//
+//  Created by لجين إبراهيم الكنهل on 03/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct TasteaProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
